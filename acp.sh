fb808d3f955a6c8f3b1d017cd6f1542e942cb582
Iwegbu Jeddy <76275625+hashkitti001@users.noreply.github.com>
2023-06-16 10:33:08 +0100

    Update acp.sh by adding PWD variable to get working directory
    Future update will get input from the user on the directory they intend to monitor

--- acp.sh
+++ acp.sh
@@ -1,7 +1,7 @@
 #ACP v.1.0 
 #By Rene Genii
 #!/bin/bash
-chmod u+x $(ls /alx*s/0x03* -Art | tail -1)
+chmod u+x $(ls $PWD -Art | tail -1)
 tasknum=$(ls | wc -l)
 git add .
 git commit -m "Added challenge $((tasknum - 2)) :tada:"
