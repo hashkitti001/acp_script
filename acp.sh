430175bcfdbc22525800dc988255b4172cb895cc
Iwegbu Jeddy <76275625+hashkitti001@users.noreply.github.com>
2023-08-07 12:16:58 +0100

    Update acp.sh to v.1.1
    v1.1 allows you enter your commit message

--- acp.sh
+++ acp.sh
@@ -1,8 +1,10 @@
-#ACP v.1.0 
-#By Rene Genii
+#ACP v.1.1
+#By Rene Geni
 #!/bin/bash
+echo "Enter your commit message: "
+read msg;
 chmod u+x $(ls $PWD -Art | tail -1)
 tasknum=$(ls | wc -l)
 git add .
-git commit -m "Added challenge $((tasknum - 2)) :tada:"
-git push
+git commit -m "${msg} :tada:"
+git push                                                                      
